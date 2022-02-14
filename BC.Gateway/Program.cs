ec59634197b034daacb709fc974b2fe876d01f68
Sputnavi <sputnavik@yandex.by>
2022-02-14 14:56:51 +0300

    Add project files.

---
+++ BC.Gateway/Program.cs
@@ -0,0 +1,39 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace BC.Gateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config
+                    .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                    .AddJsonFile(Path.Combine("Configurations", $"configuration.{hostingContext.HostingEnvironment.EnvironmentName}.json"), true, true)
+                    .AddEnvironmentVariables();
+            })
+            .ConfigureServices(s =>
+            {
+                s.AddOcelot();
+            })
+            .ConfigureLogging((hostingContext, logging) =>
+            {
+                //add your logging
+            })
+            .UseIISIntegration()
+            .Configure(app =>
+            {
+                app.UseOcelot().Wait();
+            })
+            .Build()
+            .Run();
+        }
+    } 
+}
